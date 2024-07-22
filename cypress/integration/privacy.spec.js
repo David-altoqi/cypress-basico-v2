33bc6d78b5393483a6aea69ffae3125a6a1ee746
David Goncalves <David.Silva@altoqi.com.br>
2024-07-22 09:13:19 -0300

    atualização privacy.spec.js

--- cypress/integration/privacy.spec.js
+++ cypress/integration/privacy.spec.js
@@ -1,5 +1,5 @@
 it.only('testa a página da política de privacidade de forma independente', function() {
-    cy.visit('cy/src/privacy.html')
+    cy.visit('./src/privacy.html')
 
     cy.contains('Talking About Testing').should('be.visible')
 })
